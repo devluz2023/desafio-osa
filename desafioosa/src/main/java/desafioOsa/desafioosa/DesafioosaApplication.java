d63d8e8dc1114009acfce1e5f0fd4f3046cebc92
binhojulix <fabio.jdluz@gmail.com>
2025-09-28 13:05:00 -0300

    java

---
+++ desafioosa/src/main/java/desafioOsa/desafioosa/DesafioosaApplication.java
@@ -0,0 +1,13 @@
+package desafioOsa.desafioosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioosaApplication.class, args);
+	}
+
+}
---
+++ desafioosa/src/test/java/desafioOsa/desafioosa/DesafioosaApplicationTests.java
@@ -0,0 +1,13 @@
+package desafioOsa.desafioosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
