6c941d5f2bfe8d8ac200f99ccc17bf6f873eeae7
devluz2023 <fabio.jdluz@gmail.com>
2025-09-28 23:05:02 +0000

    configuracao-redis

--- desafioosa/src/main/java/desafioOsa/desafioosa/model/Transaction.java
+++ desafioosa/src/main/java/desafioOsa/desafioosa/model/Transaction.java
@@ -1,33 +1,33 @@
-package desafioOsa.desafioosa.model;
+// package desafioOsa.desafioosa.model;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+// import java.math.BigDecimal;
+// import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
+// import org.springframework.data.annotation.Id;
 
-@Entity
-@Table(name = "transactions")
-@Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Transaction {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+// @Entity
+// @Table(name = "transactions")
+// @Builder
+// @Data
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+// public class Transaction {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+//     @ManyToOne(fetch = FetchType.LAZY)
+//     @JoinColumn(name = "user_id", nullable = false)
+//     private User user;
 
-    @Column(nullable = false)
-    private String type; // Deposit, Withdrawal, Payment
+//     @Column(nullable = false)
+//     private String type; // Deposit, Withdrawal, Payment
 
-    @Column(nullable = false)
-    private BigDecimal amount;
+//     @Column(nullable = false)
+//     private BigDecimal amount;
 
-    @Column(nullable = false)
-    private LocalDateTime date;
+//     @Column(nullable = false)
+//     private LocalDateTime date;
 
-}
+// }
