5231d834232f44f23dad660836b4fdf77471524b
devluz2023 <fabio.jdluz@gmail.com>
2025-09-28 23:38:52 +0000

    ajustes

--- desafioosa/src/main/java/desafioOsa/desafioosa/do/AccountStatementDTO.java
+++
@@ -1,7 +0,0 @@
-package desafioOsa.desafioosa.do;
-
-@Data @Builder
-public class AccountStatementDTO {
-     private BigDecimal saldoTotal;
-    private List<TransactionDTO> historico;
-}
--- desafioosa/src/main/java/desafioOsa/desafioosa/do/TransactionDTO.java
+++
@@ -1,8 +0,0 @@
-package desafioOsa.desafioosa.do;
-
-@Data @Builder
-public class TransactionDTO {
-     private String type;
-    private BigDecimal valor;
-    private String data;
-}
