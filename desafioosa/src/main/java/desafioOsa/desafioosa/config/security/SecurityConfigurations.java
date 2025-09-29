8adc8e589daf16e140ac39d2da0f8db442616a13
devluz2023 <fabio.jdluz@gmail.com>
2025-09-29 07:49:08 -0300

    configuracao-de-token

--- desafioosa/src/main/java/desafioOsa/desafioosa/config/security/SecurityConfigurations.java
+++ desafioosa/src/main/java/desafioOsa/desafioosa/config/security/SecurityConfigurations.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,7 +21,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfigurations {
    @Autowired
    SecurityFilter securityFilter;
-   
 
 
 
@@ -47,6 +48,7 @@ public class SecurityConfigurations {
                .build();
    }
  
+
    @Bean
    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
            throws Exception {
